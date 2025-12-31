02da08febcf85268360be0f5996fc70ab9c826c6
Keni <94736623+archibenz@users.noreply.github.com>
2025-12-31 11:46:36 +0300

    Improve support chat persistence

--- bot_app/states.py
+++ bot_app/states.py
@@ -1,9 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class RegistrationStates(StatesGroup):
-    waiting_for_phone = State()
-
-
 class SupportStates(StatesGroup):
     waiting_for_feedback = State()
+    in_chat = State()
